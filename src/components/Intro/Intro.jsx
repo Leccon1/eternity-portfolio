dcce12d4b49b9a0694fe177e66fd2a27b5beef57
Leccon1 <dlavania703@gamil.com>
2025-12-07 22:45:26 +0300

    feat: Animated moving of blocks to the sides has been added

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -8,26 +8,39 @@ const Intro = ({ onFinish }) => {
   const containerRef = useRef(null)
 
   useEffect(() => {
-    const title = containerRef.current.querySelector(`h1`)
+    const titleLeft = containerRef.current.querySelector(`#introTitleLeft`)
+    const titleRight = containerRef.current.querySelector(`#introTitleRight`)
+    const introContainerLeft = containerRef.current.querySelector(`#introContainerLeft`)
+    const introContainerRight = containerRef.current.querySelector(`#introContainerRight`)
 
-    animate(title, {
+    animate([titleLeft, titleRight], {
       opacity: [0, 1],
-      translateY: [40, 0],
+      translateY: [80, 0],
       easing: 'easeOutExpo',
-      duration: 1200,
+      duration: 2500,
     })
 
     createTimer({
-      duration: 800,
+      duration: 2500,
       onComplete: () => {
-        animate(title, {
+        animate([titleLeft, titleRight], {
           opacity: [1, 0],
-          easing: 'easeOutQuad',
-          duration: 600,
+          easing: 'easeInExpo',
+          duration: 2500,
           onComplete: () => {
             setVisible(false)
             onFinish()
           },
+        })
+        animate(introContainerLeft, {
+          translateX: ['0%', '-100%'],
+          easing: 'easeOutExpo',
+          duration: 2500,
+        })
+        animate(introContainerRight, {
+          translateX: ['0%', '100%'],
+          easing: 'easeOutExpo',
+          duration: 2500,
         })
       },
     })
@@ -37,7 +50,16 @@ const Intro = ({ onFinish }) => {
 
   return (
     <div className={styles.intro} ref={containerRef}>
-      <h1 className={styles.title}>Eternity</h1>
+      <div className={styles.introLeft} id="introContainerLeft">
+        <span className={styles.title} id="introTitleLeft">
+          Eter
+        </span>
+      </div>
+      <div className={styles.introRight} id="introContainerRight">
+        <span className={styles.title} id="introTitleRight">
+          nity
+        </span>
+      </div>
     </div>
   )
 }
