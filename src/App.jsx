0b5ea2e2f02c2e7125a70661b13741fdc3f7aa31
Leccon1 <dlavania703@gamil.com>
2025-12-07 21:05:15 +0300

    add sIntroFinished check to display content

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from 'react'
+
+import Intro from './components/Intro/Intro'
+
 const App = () => {
+  const [isIntroFinished, setIsIntroFinished] = useState(false)
+
   return (
-    <div className="app">
-      <h1>Мое React приложение</h1>
-      {/* Сюда перенесете вашу существующую логику */}
-    </div>
+    <>
+      {!isIntroFinished && <Intro onFinish={() => setIsIntroFinished(true)} />}
+
+      {isIntroFinished && (
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis culpa assumenda
+            saepe vero facilis a eum exercitationem in amet fugit, asperiores eos repellendus
+            similique odit esse, quod natus maxime hic!
+          </p>
+        </div>
+      )}
+    </>
   )
 }
 
