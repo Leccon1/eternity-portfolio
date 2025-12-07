47ae8216d594c78daafa8680fec40b7cd372a7bd
Leccon1 <dlavania703@gamil.com>
2025-12-07 21:39:29 +0300

    feat: Animated page appearance has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
-import { useState } from 'react'
+import { animate } from 'animejs'
+import { useEffect, useRef, useState } from 'react'
 
 import Intro from './components/Intro/Intro'
 
 const App = () => {
   const [isIntroFinished, setIsIntroFinished] = useState(false)
 
+  const containerRef = useRef(null)
+
+  useEffect(() => {
+    if (!isIntroFinished) return
+    if (!containerRef.current) return
+
+    const page = containerRef.current.querySelector(`#page`)
+    if (!page) return
+
+    animate(page, {
+      opacity: [0, 1],
+      translateY: [20, 0],
+      easing: 'easeOutQuad',
+      duration: 600,
+    })
+  }, [isIntroFinished])
+
   return (
-    <>
+    <div className="app" ref={containerRef}>
       {!isIntroFinished && <Intro onFinish={() => setIsIntroFinished(true)} />}
 
       {isIntroFinished && (
-        <div className="content">
+        <div id="page">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis culpa assumenda
-            saepe vero facilis a eum exercitationem in amet fugit, asperiores eos repellendus
-            similique odit esse, quod natus maxime hic!
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, alias nesciunt! Nulla
+            dolorum rerum similique praesentium fuga suscipit architecto recusandae, exercitationem
+            natus earum ratione esse eum quaerat quas voluptas expedita nihil officiis quia rem
+            voluptate a, nemo optio accusamus amet? Nemo doloremque veniam dolorem distinctio rem
+            quo quaerat hic aperiam dignissimos qui similique ducimus unde eligendi neque
+            voluptatum, animi sint corporis quisquam amet necessitatibus. Molestiae sapiente quaerat
+            quisquam nemo tenetur enim corporis illum alias nostrum adipisci error, in eligendi
+            pariatur fugit voluptatibus porro. Cupiditate officiis eum neque, debitis mollitia dicta
+            facere, quibusdam quam necessitatibus non doloremque accusantium molestias, vero modi.
           </p>
         </div>
       )}
-    </>
+    </div>
   )
 }
 
