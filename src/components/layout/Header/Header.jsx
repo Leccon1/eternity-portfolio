83ea18e0bd97c77cb91b4c9b7558b2667e04613c
Leccon1 <dlavania703@gamil.com>
2025-12-22 02:31:30 +0300

    feat: Links and OpenNav are written, as well as a button to open OpenNav with Styles

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,6 +1,53 @@
+import downArrowsIcon from '@icons/down-arrows.svg'
+import { useState } from 'react'
+
 import styles from './header.module.scss'
-const Header = () => {
-  return <header className={styles.header}></header>
+import navigationData from './navigation.data'
+
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <header className={styles.header}>
+      <nav
+        className={styles.navigation}
+        onMouseEnter={() => setIsOpen(true)}
+        onMouseLeave={() => setIsOpen(false)}
+      >
+        <ul className={styles.navigation__list}>
+          {navigationData.map((item) => (
+            <li key={item.label} className={styles.navigation__item}>
+              <button type="button" className={styles.navigation__link}>
+                {item.label}
+              </button>
+            </li>
+          ))}
+        </ul>
+
+        <div className={`${styles.openNav} ${isOpen ? styles.open : ''}`}>
+          <div className={styles.openNav__grid}>
+            {navigationData.map((item) => (
+              <div key={item.label} className={styles.openNav__column}>
+                {item.subMenu && (
+                  <ul className={styles.openNav__list}>
+                    {item.subMenu.map((sub) => (
+                      <li key={sub.label}>
+                        <a href={sub.href}>{sub.label}</a>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button className={styles.openNavToggle} type="button">
+          <img src={downArrowsIcon} alt="Down arrows" />
+        </button>
+      </nav>
+    </header>
+  )
 }
 
-export default Header
+export default Navigation
