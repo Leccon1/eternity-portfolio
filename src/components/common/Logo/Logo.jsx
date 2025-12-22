81ebb4a7571989660144a9f95e5c6c73a6231b45
Leccon1 <dlavania703@gamil.com>
2025-12-22 23:45:50 +0300

    feat: make the logo a link

--- src/components/common/Logo/Logo.jsx
+++ src/components/common/Logo/Logo.jsx
@@ -1,7 +1,11 @@
 import LogoImg from '@favicons/eternity.png'
 
 const Logo = () => {
-  return <img src={LogoImg} alt="logo image" style={{ width: '32px', height: 'auto' }} />
+  return (
+    <a href="/home">
+      <img src={LogoImg} alt="logo image" style={{ width: '32px', height: 'auto' }} />
+    </a>
+  )
 }
 
 export default Logo
