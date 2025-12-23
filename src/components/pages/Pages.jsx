33f85db3e75a4678a56d02f3f9b97441689797e9
Leccon1 <dlavania703@gamil.com>
2025-12-23 23:54:32 +0300

    feat: update class and added styles with 100% width

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 
 import HomeScreen from './Home/Home'
+import styles from './pages.module.scss'
 
 const Pages = () => {
   const [currentScreen, setCurrentScreen] = useState('home')
 
   const goTo = (screenName) => setCurrentScreen(screenName)
 
-  return <main className="pages-container">{currentScreen === 'home' && <HomeScreen />}</main>
+  return (
+    <main className={`${styles.pagesContainer}`}>{currentScreen === 'home' && <HomeScreen />}</main>
+  )
 }
 
 export default Pages
