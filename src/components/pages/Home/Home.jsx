bc86d18387abd1a4ca0136a4bb4bbf5f4be0db0a
Leccon1 <dlavania703@gamil.com>
2025-12-24 00:39:37 +0300

    feat: added hero content (info) with styles

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,10 +1,30 @@
+import Heading from '@common/Heading/Heading'
+
 import Hero from '../../common/Hero/Hero'
 
 import styles from './home.module.scss'
 const HomeScreen = () => {
   return (
     <div className={styles.homeScreen}>
-      <Hero />
+      <Hero className={styles.hero}>
+        <div className={styles.hero__content}>
+          <div className={styles.hero__info}>
+            <Heading className={styles.hero__title} level="h1" size="xxxl">
+              Ivan Samolkin
+            </Heading>
+            <p className={styles.hero__subtitle}>Aka Leccon1</p>
+            <p className={styles.hero__post}>Front-End Developer</p>
+            <p className={styles.hero__description}>
+              I am a self-taught front-end developer passionate about creating intuitive, modern,
+              and user-friendly interfaces.
+            </p>
+          </div>
+          <div className={styles.hero__buttons}>
+            <button type="button">View News</button>
+            <button type="button">About Me</button>
+          </div>
+        </div>
+      </Hero>
     </div>
   )
 }
