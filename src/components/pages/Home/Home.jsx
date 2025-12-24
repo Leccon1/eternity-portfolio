0492930c35607c702cf564a26ac0eacc042e9427
Leccon1 <dlavania703@gamil.com>
2025-12-24 22:26:58 +0300

    feat: add ContentContainer component for content

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import Heading from '@common/Heading/Heading'
-
-import Hero from '../../common/Hero/Hero'
+import Hero from '@common/Hero/Hero'
+import ContentContainer from '@ui/ContentContainer/ContentContainer'
 
 import styles from './home.module.scss'
 const HomeScreen = () => {
@@ -8,17 +8,19 @@ const HomeScreen = () => {
     <div className={styles.homeScreen}>
       <Hero className={styles.hero}>
         <div className={styles.hero__content}>
-          <div className={styles.hero__info}>
-            <Heading className={styles.hero__title} level="h1" size="xxxl">
-              Ivan Samolkin
-            </Heading>
-            <p className={styles.hero__subtitle}>Aka Leccon1</p>
-            <p className={styles.hero__post}>Front-End Developer</p>
-            <p className={styles.hero__description}>
-              I am a self-taught front-end developer passionate about creating intuitive, modern,
-              and user-friendly interfaces.
-            </p>
-          </div>
+          <ContentContainer>
+            <div className={styles.hero__info}>
+              <Heading className={styles.hero__title} level="h1" size="xxxl">
+                Ivan Samolkin
+              </Heading>
+              <p className={styles.hero__subtitle}>Aka Leccon1</p>
+              <p className={styles.hero__post}>Front-End Developer</p>
+              <p className={styles.hero__description}>
+                I am a self-taught front-end developer passionate about creating intuitive, modern,
+                and user-friendly interfaces.
+              </p>
+            </div>
+          </ContentContainer>
           <div className={styles.hero__buttons}>
             <button type="button">View News</button>
             <button type="button">About Me</button>
