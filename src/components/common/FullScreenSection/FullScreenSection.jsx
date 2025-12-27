8518017d56c37a2f327b69d8b61fdbcec702b43e
Leccon1 <dlavania703@gamil.com>
2025-12-27 22:29:10 +0300

    feat: add FullScreenSection compomemt with styles

---
+++ src/components/common/FullScreenSection/FullScreenSection.jsx
@@ -0,0 +1,7 @@
+import styles from './fullScreenSection.module.scss'
+
+const FullScreenSection = () => {
+  return <div className={styles.fullScreenSection}></div>
+}
+
+export default FullScreenSection
