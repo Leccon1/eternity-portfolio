1df270f14f67a8e5ce19e5530d2a496d59dc8794
Leccon1 <dlavania703@gamil.com>
2025-12-29 00:56:04 +0300

    feat: add data file with base contents

---
+++ src/components/common/Hero/hero.data.js
@@ -0,0 +1,25 @@
+export const heroData = {
+  home: {
+    title: 'Ivan Samolkin',
+    subtitle: 'Aka Leccon1',
+    post: 'Front-End Developer',
+    description:
+      'I am a self-taught front-end developer passionate about creating intuitive, modern, and user-friendly interfaces.',
+    buttons: [
+      { text: 'View News', href: '#news', variant: 'primary', onClick: () => console.log('news') },
+      {
+        text: 'About Me',
+        href: '#about',
+        variant: 'secondary',
+        onClick: () => console.log('about'),
+      },
+    ],
+  },
+  about: {
+    title: 'My Story',
+    subtitle: 'Experience',
+    post: 'Developer & Designer',
+    description: 'Here you can find more information about my professional journey and skills.',
+    buttons: [{ text: 'Download CV', href: '/cv.pdf', variant: 'primary' }],
+  },
+}
