86740fd673cc9a9a8d47c4022703e173cccd4634
Leccon1 <dlavania703@gamil.com>
2025-12-30 01:31:42 +0300

    feat: alignment of the content using ContentContainer

--- src/components/pages/Home/sections/Identify/HomeIdentify.jsx
+++ src/components/pages/Home/sections/Identify/HomeIdentify.jsx
@@ -1,18 +1,23 @@
 import Heading from '@common/Heading/Heading'
 import Text from '@common/Text/Text'
+import ContentContainer from '@ui/ContentContainer/ContentContainer'
 
 import styles from './homeIdentify.module.scss'
 const HomeIdentify = () => {
   return (
     <section className={styles.identify}>
-      <Heading level="h2" size="xl">
-        Why am I working slowly?
-      </Heading>
-      <Text fontType="primary">
-        Because every decision, from file structure to micro animations— passes through a filter of
-        expediency. I believe that design is not a separate stage, but a continuation of the project
-        architecture.
-      </Text>
+      <ContentContainer>
+        <div className={styles.identify__content}>
+          <Heading level="h2" size="xl">
+            Why am I working slowly?
+          </Heading>
+          <Text fontType="primary">
+            Because every decision, from file structure to micro animations— passes through a filter
+            of expediency. I believe that design is not a separate stage, but a continuation of the
+            project architecture.
+          </Text>
+        </div>
+      </ContentContainer>
     </section>
   )
 }
