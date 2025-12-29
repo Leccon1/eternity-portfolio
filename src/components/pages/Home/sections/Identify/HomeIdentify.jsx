ee3c4b0a42ac11e269c193d37815b78b5c880da1
Leccon1 <dlavania703@gamil.com>
2025-12-30 00:30:20 +0300

    feat: add title section

--- src/components/pages/Home/sections/Identify/HomeIdentify.jsx
+++ src/components/pages/Home/sections/Identify/HomeIdentify.jsx
@@ -1,7 +1,14 @@
-import styles from './homeIdentify.module.scss'
+import Heading from '@common/Heading/Heading'
 
+import styles from './homeIdentify.module.scss'
 const HomeIdentify = () => {
-  return <section className={styles.identify}>Identify</section>
+  return (
+    <section className={styles.identify}>
+      <Heading level="h2" size="xl">
+        Why am I working slowly?
+      </Heading>
+    </section>
+  )
 }
 
 export default HomeIdentify
