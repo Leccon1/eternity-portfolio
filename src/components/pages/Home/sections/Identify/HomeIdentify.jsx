350a20766133beaaae9f0b7d238c481cd3fabe0e
Leccon1 <dlavania703@gamil.com>
2025-12-30 01:16:04 +0300

    feat: added content text

--- src/components/pages/Home/sections/Identify/HomeIdentify.jsx
+++ src/components/pages/Home/sections/Identify/HomeIdentify.jsx
@@ -1,4 +1,5 @@
 import Heading from '@common/Heading/Heading'
+import Text from '@common/Text/Text'
 
 import styles from './homeIdentify.module.scss'
 const HomeIdentify = () => {
@@ -7,6 +8,11 @@ const HomeIdentify = () => {
       <Heading level="h2" size="xl">
         Why am I working slowly?
       </Heading>
+      <Text fontType="primary">
+        Because every decision, from file structure to micro animations— passes through a filter of
+        expediency. I believe that design is not a separate stage, but a continuation of the project
+        architecture.
+      </Text>
     </section>
   )
 }
