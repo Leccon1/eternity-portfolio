2ea4ddb32dbe65c92b7bc37087a484cb526217d0
Leccon1 <dlavania703@gamil.com>
2026-01-01 04:10:21 +0300

    refactor: importn component InsightSection and change code

--- src/components/pages/Home/sections/Identify/HomeIdentify.jsx
+++ src/components/pages/Home/sections/Identify/HomeIdentify.jsx
@@ -1,25 +1,14 @@
-import Heading from '@common/Heading/Heading'
-import NavButton from '@common/NavButton/NavButton'
-import Text from '@common/Text/Text'
-import ContentContainer from '@ui/ContentContainer/ContentContainer'
+import InsightSection from '@common/InsightSection/InsightSection'
 
 import styles from './homeIdentify.module.scss'
 const HomeIdentify = () => {
   return (
     <section className={styles.identify}>
-      <ContentContainer>
-        <div className={styles.identify__content}>
-          <Heading level="h2" size="xl">
-            Why am I working slowly?
-          </Heading>
-          <Text fontType="primary" className={styles.identify__description}>
-            Because every decision, from file structure to micro animations— passes through a filter
-            of expediency. I believe that design is not a separate stage, but a continuation of the
-            project architecture.
-          </Text>
-          <NavButton variant="secondary">View the history of my formation...</NavButton>
-        </div>
-      </ContentContainer>
+      <InsightSection
+        title="Why am I working slowly?"
+        description="Because every decision, from file structure to micro animations— passes through a filter of expediency. I believe that design is not a separate stage, but a continuation of the project architecture."
+        buttonText="View the history of my formation..."
+      />
     </section>
   )
 }
