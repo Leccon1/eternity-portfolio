8deb0d5f18fb9f5945c17e635e3610532e99b4bc
Leccon1 <dlavania703@gamil.com>
2026-01-01 04:55:55 +0300

    feat: connected Projects component

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import FullScreenSection from '../../common/FullScreenSection/FullScreenSection'
 
 import styles from './home.module.scss'
-import Identify from './sections/Identify/Identify'
+import Identify from './sections/Identify/HomeIdentify'
+import Projects from './sections/Projects/Projects'
+
 const HomeScreen = () => {
   return (
     <div className={styles.homeScreen}>
       <FullScreenSection>
         <Identify />
+        <Projects />
       </FullScreenSection>
     </div>
   )
