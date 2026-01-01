ac0a45ecff53318002275904d337323c951a40e5
Leccon1 <dlavania703@gamil.com>
2026-01-01 05:05:06 +0300

    refactor: add prop for overflow

--- src/components/common/FullScreenSection/FullScreenSection.jsx
+++ src/components/common/FullScreenSection/FullScreenSection.jsx
@@ -1,7 +1,11 @@
 import styles from './fullScreenSection.module.scss'
 
-const FullScreenSection = ({ children }) => {
-  return <div className={styles.fullScreenSection}>{children}</div>
+const FullScreenSection = ({ children, overflow = 'hidden' }) => {
+  return (
+    <div className={styles.fullScreenSection} style={{ overflow }}>
+      {children}
+    </div>
+  )
 }
 
 export default FullScreenSection
