5dbd0458e90dffab02aab64890b6d63e4d949631
Leccon1 <dlavania703@gamil.com>
2026-01-03 02:44:21 +0300

    refactor: change name improt and added container for content

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -2,11 +2,16 @@ import FullScreenSection from '../../common/FullScreenSection/FullScreenSection'
 
 import styles from './home.module.scss'
 import Identify from './sections/Identify/Identify'
+import Projects from './sections/Projects/Projects'
+
 const HomeScreen = () => {
   return (
     <div className={styles.homeScreen}>
       <FullScreenSection>
-        <Identify />
+        <div className={styles.homeScreen__content}>
+          <Identify />
+          <Projects />
+        </div>
       </FullScreenSection>
     </div>
   )
