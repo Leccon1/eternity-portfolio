d7f02152395cf4cccb8b44a20985d64c1ddb95b5
Leccon1 <dlavania703@gamil.com>
2026-01-09 01:06:36 +0300

    feat: added data file for content in ProjectsCard component

---
+++ src/components/common/ProjectsCard/projects-card.data.js
@@ -0,0 +1,13 @@
+export const projectsData = [
+  {
+    id: 1,
+    title: 'Bookmark manager',
+    description:
+      'An interactive web application with a dynamic interface and a thoughtful user scenario, focused on convenience and a holistic UX.',
+    techStack: ['Html', 'SCSS', 'JavaScript', 'Vite'],
+    image: new URL('@images/previewProjects/bookmark-manager-preview.png', import.meta.url).href,
+    link: '#',
+  },
+]
+
+export default projectsData
