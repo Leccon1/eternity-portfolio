d9ae8ec9d683305fa771a7da6be0f0c343167e4e
Leccon1 <dlavania703@gamil.com>
2026-01-10 00:49:40 +0300

    refactor: remove parent tag and move class name in InsightSection

--- src/components/pages/Home/sections/Identify/Identify.jsx
+++ src/components/pages/Home/sections/Identify/Identify.jsx
@@ -4,13 +4,12 @@ import NavButton from '@common/NavButton/NavButton'
 import styles from './identify.module.scss'
 const Identify = () => {
   return (
-    <section className={styles.identify}>
-      <InsightSection
-        title="Why am I working slowly?"
-        description="Because every decision, from file structure to micro animations— passes through a filter of expediency. I believe that design is not a separate stage, but a continuation of the project architecture."
-        action={<NavButton variant="secondary">View the history of my formation...</NavButton>}
-      />
-    </section>
+    <InsightSection
+      className={styles.identify}
+      title="Why am I working slowly?"
+      description="Because every decision, from file structure to micro animations— passes through a filter of expediency. I believe that design is not a separate stage, but a continuation of the project architecture."
+      action={<NavButton variant="secondary">View the history of my formation...</NavButton>}
+    />
   )
 }
 
