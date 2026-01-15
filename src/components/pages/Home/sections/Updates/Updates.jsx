0aeb89e9349983fd01283ef3a67b57baddef7a61
Leccon1 <dlavania703@gamil.com>
2026-01-16 00:34:42 +0300

    chore: connect news article

--- src/components/pages/Home/sections/Updates/Updates.jsx
+++ src/components/pages/Home/sections/Updates/Updates.jsx
@@ -1,7 +1,12 @@
+import News from './News/News'
 import styles from './Updates.module.scss'
 
 const Updates = ({ className = '' }) => {
-  return <section className={`${styles.Updates} ${className}`}>NEWS</section>
+  return (
+    <section className={`${styles.Updates} ${className}`}>
+      <News />
+    </section>
+  )
 }
 
 export default Updates
