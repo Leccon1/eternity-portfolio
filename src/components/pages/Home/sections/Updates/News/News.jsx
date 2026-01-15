1d37154c785927115c369b7290ca10018d52279d
Leccon1 <dlavania703@gamil.com>
2026-01-16 00:59:09 +0300

    refactor: taking out content in newsItem.data.js file

--- src/components/pages/Home/sections/Updates/News/News.jsx
+++ src/components/pages/Home/sections/Updates/News/News.jsx
@@ -1,28 +1,25 @@
-import img from '@images/previewProjects/bookmark-manager-preview.png'
-
 import styles from './news.module.scss'
+import { newsData } from './newsItem.data.js'
+
 const News = () => {
   return (
     <section className={styles.news}>
       <p className={styles.newsTitle}>News</p>
-      <article
-        className={styles.item}
-        onMouseEnter={(e) => e.currentTarget.classList.add(styles.itemHover)}
-        onMouseLeave={(e) => e.currentTarget.classList.remove(styles.itemHover)}
-      >
-        <div className={styles.itemContent}>
-          <div className={styles.itemImage}>
-            <img src={img} alt="" />
-            <div className={styles.itemImageOverlay}>
-              <span>Quick View</span>
+
+      {newsData.map((item) => (
+        <article key={item.id} className={styles.item}>
+          <div className={styles.itemContent}>
+            <div className={styles.itemImage}>
+              <img src={item.image} alt={item.title} />
+              <div className={styles.itemImageOverlay}>
+                <span>Quick View</span>
+              </div>
             </div>
+            <p className={styles.itemTitle}>{item.title}</p>
+            <p className={styles.itemDate}>{item.date}</p>
           </div>
-          <p className={styles.itemTitle}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, facilis!
-          </p>
-          <p className={styles.itemDate}>OCT 28, 2026</p>
-        </div>
-      </article>
+        </article>
+      ))}
     </section>
   )
 }
---
+++ src/components/pages/Home/sections/Updates/News/newsItem.data.js
@@ -0,0 +1,11 @@
+export const newsData = [
+  {
+    id: 1,
+    image: '',
+    title: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, facilis!',
+    date: 'OCT 28, 2026',
+    link: '#',
+  },
+]
+
+export default newsData
