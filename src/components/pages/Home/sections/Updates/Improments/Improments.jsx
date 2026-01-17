b9e12fd9affd6670522b599933e01f0926730a16
Leccon1 <dlavania703@gamil.com>
2026-01-18 01:31:01 +0300

    feat(Improments): added overlay content and update classes name

--- src/components/pages/Home/sections/Updates/Improments/Improments.jsx
+++ src/components/pages/Home/sections/Updates/Improments/Improments.jsx
@@ -1,14 +1,27 @@
+import NavButton from '@common/NavButton/NavButton'
+
 import styles from './improments.module.scss'
 
 const Improments = () => {
   return (
     <article className={styles.improments}>
-      <p className={styles.version}>v1.0.0</p>
-      <ul>
-        <li>Lorem ipsum dolor sit amet.</li>
-        <li>Lorem ipsum dolor sit amet.</li>
-        <li>Lorem ipsum dolor sit amet.</li>
+      <p className={styles.impromentsVersion}>v1.0.0</p>
+
+      <ul className={styles.impromentsList}>
+        <li className={styles.impromentsItem}>Lorem ipsum dolor sit amet.</li>
+        <li className={styles.impromentsItem}>Lorem ipsum dolor sit amet.</li>
+        <li className={styles.impromentsItem}>Lorem ipsum dolor sit amet.</li>
       </ul>
+
+      <div className={styles.impromentsOverlay}>
+        <div className={styles.impromentsProgressBar}>
+          <div className={styles.impromentsProgress}></div>
+        </div>
+
+        <div className={styles.impromentsProgressStatus}>85% completed</div>
+
+        <NavButton variant="secondary">Detailed log</NavButton>
+      </div>
     </article>
   )
 }
