a98248e15297de0ca4460c819875e14ecb385493
Leccon1 <dlavania703@gamil.com>
2026-01-18 02:25:14 +0300

    feat(HomeScreen): connect Updates section

--- src/components/pages/Home/HomeScreen.jsx
+++ src/components/pages/Home/HomeScreen.jsx
@@ -3,15 +3,20 @@ import FullScreenSection from '../../common/FullScreenSection/FullScreenSection'
 import styles from './homeScreen.module.scss'
 import Identify from './sections/Identify/Identify'
 import Projects from './sections/Projects/Projects'
+import Updates from './sections/Updates/Updates'
 
 const HomeScreen = () => {
   return (
     <section className={styles.homeScreen}>
       <FullScreenSection>
-        {/* <div className={styles.homeScreen__pitch}>
+        <div className={styles.homeScreen__pitch}>
           <Identify />
           <Projects />
-        </div> */}
+        </div>
+      </FullScreenSection>
+
+      <FullScreenSection>
+        <Updates />
       </FullScreenSection>
     </section>
   )
