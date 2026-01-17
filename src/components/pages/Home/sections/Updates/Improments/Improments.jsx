77822b6a8c73ddbfbb6e095bba3cdc17ad10181d
Leccon1 <dlavania703@gamil.com>
2026-01-18 01:48:46 +0300

    refactor(Improvements): changed name from Improments to Improvements

--- src/components/pages/Home/sections/Updates/Improments/Improments.jsx
+++ src/components/pages/Home/sections/Updates/Improments/Improments.jsx
@@ -4,22 +4,22 @@ import styles from './improments.module.scss'
 
 const Improments = () => {
   return (
-    <article className={styles.improments}>
-      <div className={styles.impromentsContent}>
-        <p className={styles.impromentsVersion}>v1.0.0</p>
-        <ul className={styles.impromentsList}>
-          <li className={styles.impromentsItem}>Lorem ipsum dolor sit amet.</li>
-          <li className={styles.impromentsItem}>Lorem ipsum dolor sit amet.</li>
-          <li className={styles.impromentsItem}>Lorem ipsum dolor sit amet.</li>
+    <article className={styles.Improvements}>
+      <div className={styles.ImprovementsContent}>
+        <p className={styles.ImprovementsVersion}>v1.0.0</p>
+        <ul className={styles.iImprovementsList}>
+          <li className={styles.ImprovementsItem}>Lorem ipsum dolor sit amet.</li>
+          <li className={styles.ImprovementsItem}>Lorem ipsum dolor sit amet.</li>
+          <li className={styles.ImprovementsItem}>Lorem ipsum dolor sit amet.</li>
         </ul>
       </div>
 
-      <div className={styles.impromentsOverlay}>
-        <div className={styles.impromentsProgressBar}>
-          <div className={styles.impromentsProgress}></div>
+      <div className={styles.ImprovementsOverlay}>
+        <div className={styles.ImprovementsProgressBar}>
+          <div className={styles.ImprovementsProgress}></div>
         </div>
 
-        <div className={styles.impromentsProgressStatus}>85% completed</div>
+        <div className={styles.ImprovementsProgressStatus}>85% completed</div>
 
         <NavButton variant="secondary">Detailed log</NavButton>
       </div>
