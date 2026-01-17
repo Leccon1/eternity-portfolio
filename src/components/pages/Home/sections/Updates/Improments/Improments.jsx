ca0bf3f012344832d6b00a9087342e955664cab1
Leccon1 <dlavania703@gamil.com>
2026-01-18 01:20:34 +0300

    feat(Improments): added content (no hover)

--- src/components/pages/Home/sections/Updates/Improments/Improments.jsx
+++ src/components/pages/Home/sections/Updates/Improments/Improments.jsx
@@ -1,7 +1,16 @@
 import styles from './improments.module.scss'
 
 const Improments = () => {
-  return <article className={styles.improments}></article>
+  return (
+    <article className={styles.improments}>
+      <p className={styles.version}>v1.0.0</p>
+      <ul>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+      </ul>
+    </article>
+  )
 }
 
 export default Improments
