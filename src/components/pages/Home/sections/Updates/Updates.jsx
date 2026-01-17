a49fa3ce2c8568bc5223c43297ad9e2395baf576
Leccon1 <dlavania703@gamil.com>
2026-01-18 01:19:55 +0300

    feat(Updates-section): connect Improments compoent

--- src/components/pages/Home/sections/Updates/Updates.jsx
+++ src/components/pages/Home/sections/Updates/Updates.jsx
@@ -1,3 +1,4 @@
+import Improments from './Improments/Improments'
 import News from './News/News'
 import styles from './Updates.module.scss'
 
@@ -5,6 +6,7 @@ const Updates = ({ className = '' }) => {
   return (
     <section className={`${styles.Updates} ${className}`}>
       <News />
+      <Improments />
     </section>
   )
 }
