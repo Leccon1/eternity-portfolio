2828ed1624802bb6282e1f9774437a00f2068b78
Leccon1 <dlavania703@gamil.com>
2026-01-25 00:59:03 +0300

    feat: added base engine for particles and base styles particles

--- src/components/common/Hero/Hero.jsx
+++ src/components/common/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import Heading from '@common/Heading/Heading'
 import NavButton from '@common/NavButton/NavButton'
 import { useAnimation } from '@hooks/useAnimationContext'
 import ContentContainer from '@ui/ContentContainer/ContentContainer'
-import { animate, createTimeline, splitText, stagger } from 'animejs'
+import { animate, createTimeline, splitText, stagger, engine, utils } from 'animejs'
 import { useEffect, useRef } from 'react'
 
 import styles from './hero.module.scss'
@@ -10,6 +10,37 @@ import styles from './hero.module.scss'
 const Hero = ({ data }) => {
   const containerRef = useRef(null)
   const { state } = useAnimation()
+
+  useEffect(() => {
+    const [$container] = utils.$('.container')
+    const [$range] = utils.$('.range')
+
+    if (!$container || !$range) return
+
+    for (let i = 0; i < 150; i++) {
+      const $particle = document.createElement('div')
+      $particle.classList.add(styles.particle)
+      $container.appendChild($particle)
+
+      animate($particle, {
+        x: utils.random(-10, 10, 2) + 'rem',
+        y: utils.random(-3, 3, 2) + 'rem',
+        scale: [{ from: 0, to: 1 }, { to: 0 }],
+        delay: utils.random(0, 1000),
+        loop: true,
+      })
+    }
+
+    const onInput = (e) => {
+      utils.sync(() => (engine.speed = e.target.value))
+    }
+
+    $range.addEventListener('input', onInput)
+
+    return () => {
+      $range.removeEventListener('input', onInput)
+    }
+  }, [])
 
   useEffect(() => {
     if (!state.introFinished || !data) return
@@ -66,6 +97,14 @@ const Hero = ({ data }) => {
         <div className={`${styles.glow} ${styles.glow_2}`} />
         <div className={`${styles.glow} ${styles.glow_3}`} />
         <div className={`${styles.glow} ${styles.glow_4}`} />
+        <div className={styles.cont}>
+          <div className="large row container"></div>
+          <div className="medium row">
+            <fieldset className="controls">
+              <input type="range" min="0.1" max="2" value="1" step=".01" className="range" />
+            </fieldset>
+          </div>
+        </div>
       </div>
 
       <div className={styles.hero__content} ref={containerRef}>
